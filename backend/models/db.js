f0e89503f18f00130b1d26d871e248049e22f09a
Laxingo <goncalosilchaves@gmail.com>
2025-06-06 16:55:37 +0100

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
   require('dotenv').config();
+=======
+require('dotenv').config();
+>>>>>>> Stashed changes
   const { Sequelize, DataTypes } = require('sequelize');
 
   // Connect using env vars
@@ -13,6 +17,7 @@
     }
   );
 
+<<<<<<< Updated upstream
   // Import models
   const Utilizador = require('./utilizador.model')(sequelize, DataTypes);
   const Atividades = require('./atividades.model')(sequelize, DataTypes);
@@ -42,3 +47,53 @@ sequelize.sync({ force: true })
   });
 
   module.exports = db;
+=======
+// IMPORTAÇÃO DOS MODELS
+const Utilizador = require('./utilizador.model')(sequelize, DataTypes);
+const Atividades = require('./atividades.model')(sequelize, DataTypes);
+const Fotos = require('./fotos.model')(sequelize, DataTypes);
+const Inscritos = require('./inscritos.model')(sequelize, DataTypes);
+const Medalhas = require('./medalhas.model')(sequelize, DataTypes);
+const Reuniao = require('./reuniao.model')(sequelize, DataTypes);
+
+// CRIAR OBJETO DB
+  // Collect models
+  const db = {
+    sequelize,
+    Sequelize,
+    Utilizador,
+    Atividades,
+    Fotos,
+    Inscritos,
+    Medalhas,
+    Reuniao,
+  };
+
+Object.entries(db).forEach(([key, model]) => {
+  if (typeof model === 'object' && model !== null && model.associate instanceof Function) {
+    model.associate(db);
+  }
+});
+
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log('✅ Tabelas recriadas com sucesso.');
+  })
+  .catch(err => {
+    console.error('❌ Erro ao criar as tabelas:', err);
+  });
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+
+
+module.exports = db;
+
+>>>>>>> Stashed changes
