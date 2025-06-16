b00a9de3d05ca332620322de7cba253fba234918
Sdanielgg <40220220@esmad.ipp.pt>
2025-06-16 14:34:44 +0100

    botão de mostrar formulario para alterar os detalhes e logout

--- frontend/js/perfil.js
+++ frontend/js/perfil.js
@@ -1,7 +1,6 @@
-
 const utilizador = JSON.parse(localStorage.getItem('utilizador'));
 
-console.log(utilizador.id)
+
 async function fetchUtilizadorDetails() {
     try {
         const response = await fetch(`http://127.0.1:3000/Utilizador/${utilizador.id}`);
@@ -9,13 +8,13 @@ async function fetchUtilizadorDetails() {
             throw new Error('Erro ao buscar detalhes do utilizador');
         }
         const data = await response.json();
-        console.log('Detalhes do utilizador:', data);
-        const utilizadorLoggedIn =document.getElementById('nomeUtilizadorLoggedIn');
+        const utilizadorLoggedIn = document.getElementById('nomeUtilizadorLoggedIn');
         utilizadorLoggedIn.textContent = data.nomeUtilizador;
     } catch (error) {
         console.error('Erro ao carregar detalhes do utilizador:', error);
         alert('Erro ao carregar detalhes do utilizador: ' + error.message);
-    }}
+    }
+}
 fetchUtilizadorDetails();
 
 document.getElementById('updateForm').addEventListener('submit', async (e) => {
@@ -23,7 +22,10 @@ document.getElementById('updateForm').addEventListener('submit', async (e) => {
 
     const nome = document.getElementById('name').value;
     const password = document.getElementById('password').value;
-
+    if (nome== '' || password == '') {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
     try {
         const response = await fetch(`http://127.0.1:3000/Utilizador/${utilizador.id}`, {
             method: 'PUT',
@@ -48,3 +50,22 @@ document.getElementById('updateForm').addEventListener('submit', async (e) => {
     }
 }
 );
+
+//  Para esconder o formulario para mudar os detalhes do utilizador
+const updateFormBtn = document.getElementById('updateFormBtn');
+updateFormBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const updateForm = document.getElementById('updateForm');
+    updateForm.style.display = 'block';
+    updateFormBtn.style.display = 'none';
+        
+});
+
+// Botao para o logout
+
+document.getElementById('logoutBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    localStorage.removeItem('utilizador');
+    window.location.href = 'login.html';
+});
